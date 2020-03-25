62dc6e91df79ca9c584b5b8abe671214d891a1ec
HenrikDK2 <Henrik265@outlook.dk>
2020-03-25 23:33:24 +0100

    Sidste commit for i dag

---
+++ src/javascript/slider.js
@@ -0,0 +1,24 @@
+const articles = document.querySelectorAll('.article');
+const regex = /[-?\d]{0,}/gm;
+articles.forEach(article => {
+  let left;
+  let startX;
+  article.addEventListener('touchstart', (e) => {
+    left = e.currentTarget.style.left.match(regex)[0];
+    startX = e.touches[0].screenX;
+  })
+
+  article.addEventListener('touchmove', (e) => {
+    if (left === "") left = 0;
+    let newX = (startX + parseInt(left)) - e.touches[0].screenX;
+
+    if (newX < -110) {
+      newX = -128;
+    }
+
+    if(newX > 0){
+      newX = 0;
+    }
+    article.style.left = newX + "PX";
+  });
+});
