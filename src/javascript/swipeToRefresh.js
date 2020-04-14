4914e0e0a82a5bdd3c228edc7788c123c68a39ef
HenrikDK2 <Henrik265@outlook.dk>
2020-04-14 16:53:43 +0200

    Style Refresh

--- src/javascript/swipeToRefresh.js
+++ src/javascript/swipeToRefresh.js
@@ -1,7 +1,20 @@
 const ptr = PullToRefresh.init({
   mainElement: 'body',
+  iconArrow: `<div id="floatingCirclesG">
+	<div class="f_circleG" id="frotateG_01"></div>
+	<div class="f_circleG" id="frotateG_02"></div>
+	<div class="f_circleG" id="frotateG_03"></div>
+	<div class="f_circleG" id="frotateG_04"></div>
+	<div class="f_circleG" id="frotateG_05"></div>
+	<div class="f_circleG" id="frotateG_06"></div>
+	<div class="f_circleG" id="frotateG_07"></div>
+	<div class="f_circleG" id="frotateG_08"></div>
+</div>`,
+iconRefreshing:"<div><div>",
+distReload:40,
+instructionsReleaseToRefresh:"Release to refresh news",
   onRefresh() {
-    categoryList.innerHTML ="";
+    categoryList.innerHTML = "";
     getArticles();
   }
 });
